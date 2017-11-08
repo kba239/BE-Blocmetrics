623ac1e034bd4f35e41d44d3c16140b553ba3a5a
kba239 <adams.kate.b@gmail.com>
2017-11-08 08:48:01 -0800

    Added API Controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
 end
